0b7a8f326a02e87cae8d98d6d87a0c8dfc80e2ac
Anthyy <42423921+Anthyy@users.noreply.github.com>
2018-09-07 12:05:37 +1000

    Inheritance with Weapons & Projectiles

---
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Weapon
+{
+    public float spread = 6;
+
+    public override void Attack()
+    {
+        // Store forward direction of player
+        Vector3 direction = transform.forward;
+        // Calculate spread by using range
+        Vector3 spread = Vector3.zero;
+        // Offset on Local Y
+        spread += transform.up* Random.Range(-accuracy, accuracy);
+        // Offset on local X
+        spread += transform.right* Random.Range(-accuracy, accuracy);
+        // Instantiate a new bullet from prefab "projectile"
+        GameObject clone = Instantiate(projectile, transform.position, transform.rotation);
+        // Get the component from the new bullet
+        Bullet newBullet = clone.GetComponent<Bullet>();
+        // Tell the bullet to Fire()
+        newBullet.Fire(direction + spread);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
